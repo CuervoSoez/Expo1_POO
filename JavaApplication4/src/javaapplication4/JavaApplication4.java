e345247a21a4dd45aef0e3ba63d700749905f80f
estudiante <estudiante@10.220.4.109>
2025-05-22 17:51:26 -0500

    HashMap Codigo

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -4,6 +4,9 @@
  */
 package javaapplication4;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author estudiante
@@ -13,8 +16,38 @@ public class JavaApplication4 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+        public static void main(String[] args) {
+  
+        HashMap<String, Double> menu = new HashMap<>();
+
+  
+        menu.put("Hamburguesa Clásica", 8.99);
+        menu.put("Pizza Margarita", 12.50);
+        menu.put("Ensalada César", 6.75);
+        menu.put("Sopa del día", 4.25);
+        menu.put("Postre de la casa", 3.50);
+        menu.put("Postre de la TIENDA", 3.50);
+
+        System.out.println("MENÚ DEL RESTAURANTE:");
+        for (Map.Entry<String, Double> item : menu.entrySet()) {
+            System.out.println(item.getKey() + " - $" + item.getValue());
+        }
+
+
+        String platillo = "Pizza Margarita";
+        if (menu.containsKey(platillo)) {
+            System.out.println("El precio de " + platillo + " es $" + menu.get(platillo));
+        } else {
+            System.out.println("\n" + platillo + " no está disponible en el menú.");
+        }
+
+ 
+        menu.remove("Hamburguesa Clásica");
+
+        System.out.println("MENÚ ACTUALIZADO:");
+        for (Map.Entry<String, Double> item : menu.entrySet()) {
+            System.out.println(item.getKey() + " - $" + item.getValue());
+        }
     }
-    
 }
+
